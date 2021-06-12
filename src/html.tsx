55570a605ce3557663826de9e2c360b1b5ed0ecd
Jens van Hellemondt <me@jvh.one>
2021-06-12 11:15:17 +0200

    chore: linting

--- src/html.tsx
+++ src/html.tsx
@@ -1,32 +1,38 @@
-import React, {ReactElement} from 'react'
+import React, { ReactElement } from 'react';
 
 interface IProps {
-    htmlAttributes: {
-        lang: string
-    }
-    headComponents: ReactElement[]
-    preBodyComponents: ReactElement[]
-    body: string
-    postBodyComponents: ReactElement[]
+  htmlAttributes: {
+    lang: string
+  }
+  headComponents: ReactElement[]
+  preBodyComponents: ReactElement[]
+  body: string
+  postBodyComponents: ReactElement[]
 }
 
 const HTML = (props: IProps): ReactElement => {
-    const {htmlAttributes, headComponents, preBodyComponents, body, postBodyComponents} = props
+  const {
+    htmlAttributes,
+    headComponents,
+    preBodyComponents,
+    body,
+    postBodyComponents,
+  } = props;
 
-    return (
-        <html lang={htmlAttributes.lang}>
-        <head>
-            <meta charSet="utf-8"/>
-            <meta httpEquiv="x-ua-compatible" content="ie=edge"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin=""/>
-            {headComponents}
-        </head>
+  return (
+    <html lang={htmlAttributes.lang}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="" />
+        {headComponents}
+      </head>
 
-        <body className="light">
+      <body className="light">
         <script
-            dangerouslySetInnerHTML={{
-                __html: `
+          dangerouslySetInnerHTML={{
+            __html: `
               (function() {
                 window.__onThemeChange = function() {};
                 function setTheme(newTheme) {
@@ -52,14 +58,14 @@ const HTML = (props: IProps): ReactElement => {
                 setTheme(preferredTheme || (darkQuery.matches ? 'dark' : 'light'));
               })();
             `,
-            }}
+          }}
         />
         {preBodyComponents}
-        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{__html: body}}/>
+        <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
         {postBodyComponents}
-        </body>
-        </html>
-    )
-}
+      </body>
+    </html>
+  );
+};
 
-export default HTML
+export default HTML;
