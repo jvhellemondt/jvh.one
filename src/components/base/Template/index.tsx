abee944dee593153ebe24db3f6a94e71754f6205
Jens van Hellemondt <me@jvh.one>
2021-06-13 20:14:55 +0200

    feat(base): template components

---
+++ src/components/base/Template/index.tsx
@@ -0,0 +1,46 @@
+import React, { ComponentPropsWithRef, ReactElement, ReactNode } from 'react';
+import * as style from './style.module.scss';
+
+export const Main = (props: ComponentPropsWithRef<'main'>): ReactElement => {
+  const { className } = props;
+  const classNames = [style.main, className].filter(Boolean);
+  return (
+    <main {...props} className={classNames.join(' ')} />
+  );
+};
+
+export const Container = (props: ComponentPropsWithRef<'div'>): ReactElement => {
+  const { className } = props;
+  const classNames = [style.container, className].filter(Boolean);
+  return (
+    <div {...props} className={classNames.join(' ')} />
+  );
+};
+
+export const Section = (props: React.ComponentPropsWithRef<'section'>): ReactElement => {
+  const { className } = props;
+  const classNames = [style.section, className].filter(Boolean);
+  return (
+    <section {...props} className={classNames.join(' ')} />
+  );
+};
+
+// export const Header = (props: { children: ReactNode }) => (
+//   <header>
+//     <Container className={style.header}>
+//       {props.children}
+//     </Container>
+//   </header>
+// );
+//
+export const Block = (props: { children: ReactNode }): ReactElement => (
+  <Section>
+    <Container {...props} />
+  </Section>
+);
+
+export const Content = (props: { children: ReactNode }): ReactElement => (
+  <Section>
+    <Container className={style.content} {...props} />
+  </Section>
+);
