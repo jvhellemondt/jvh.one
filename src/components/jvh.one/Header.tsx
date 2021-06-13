9b4856073bc72939b6c7991de9c37820e9565d92
Jens van Hellemondt <me@jvh.one>
2021-06-13 20:16:36 +0200

    chore: linting

--- src/components/jvh.one/Header.tsx
+++ src/components/jvh.one/Header.tsx
@@ -1,9 +1,10 @@
-import * as React from 'react'
-import Toggle from "./Toggle";
-import './Header.scss'
-import { Helmet } from "react-helmet";
-import { graphql, Link, StaticQuery } from "gatsby";
-import { LinkGetProps } from 'reach__router'
+import * as React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import { LinkGetProps } from 'reach__router';
+import { Helmet } from 'react-helmet';
+import { graphql, Link, StaticQuery } from 'gatsby';
+import Toggle from './Toggle';
+import './Header.scss';
 
 interface IState {
   theme: string
@@ -21,52 +22,69 @@ const query = graphql`
 
 export default class Header extends React.Component<Record<string, unknown>, IState> {
   constructor(props: Record<string, unknown>) {
-    super(props)
-    this.state = { theme: '' }
-    this.handleThemeChange = this.handleThemeChange.bind(this)
+    super(props);
+    this.state = { theme: '' };
+    this.handleThemeChange = this.handleThemeChange.bind(this);
   }
-  
-  componentDidMount() {
+
+  componentDidMount(): void {
     // @ts-ignore
+    // eslint-disable-next-line no-undef
     this.setState({ theme: window.__theme });
     // @ts-ignore
+    // eslint-disable-next-line no-undef
     window.__onThemeChange = () => {
       // @ts-ignore
+      // eslint-disable-next-line no-undef
       this.setState({ theme: window.__theme });
     };
   }
-  
-  handleThemeChange() {
+
+  handleThemeChange(): void {
+    const { theme } = this.state;
     // @ts-ignore
+    // eslint-disable-next-line no-undef
     window.__setPreferredTheme(
-      this.state.theme === 'dark' ? 'light' : 'dark'
-    )
+      theme === 'dark' ? 'light' : 'dark'
+    );
   }
-  
+
   render(): React.ReactNode {
+    const { theme } = this.state;
     return (
       <StaticQuery
-        query={ query }
-        render={ data => {
+        query={query}
+        render={(data) => {
           const { siteMetadata } = data.site;
-          const isHome = (props: LinkGetProps): boolean => props.isCurrent
+          const isHome = (props: LinkGetProps): boolean => props.isCurrent;
           return (
             <>
-              <Helmet meta={ [ { name: 'theme-color', content: this.state.theme, }, ] }/>
+              <Helmet meta={[{
+                name: 'theme-color',
+                content: theme
+              }]}
+              />
               <header className="header header__container">
                 {
-                  !isHome && <div className="header header__left">
-                    <Link to={ "/" } getProps={ isHome }> &larr; { siteMetadata.title } </Link>
-                  </div>
+                  !isHome && (
+                    <div className="header header__left">
+                      <Link to="/" getProps={isHome}>
+                        {' '}
+                        &larr;
+                        {siteMetadata.title}
+                      </Link>
+                    </div>
+                  )
                 }
-                
+
                 <div className="header header__right">
-                  <Toggle handleClick={ this.handleThemeChange } dark={ this.state.theme === 'dark' }/>
+                  <Toggle handleClick={this.handleThemeChange} dark={this.state.theme === 'dark'} />
                 </div>
               </header>
             </>
-          )
-        } }/>
-    )
+          );
+        }}
+      />
+    );
   }
 }
