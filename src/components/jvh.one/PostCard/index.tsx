5a35e58631e9ea208294d351143e1f1301eecd23
Jens van Hellemondt <me@jvh.one>
2021-06-14 08:50:29 +0200

    feat(post-card): init

--- src/components/jvh.one/PostCard/index.tsx
+++ src/components/jvh.one/PostCard/index.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { ComponentPropsWithRef } from 'react';
 
-export type IndexProps = {}
+import { GatsbyImage, GatsbyImageProps } from 'gatsby-plugin-image';
+import * as style from './style.module.scss';
 
-const Index = (props: IndexProps): React.ReactElement => {
-  // eslint-disable-next-line no-console
-  console.log({
-    IndexProps: props
-  });
+export type PostCardProps = ComponentPropsWithRef<'div'>
 
-  return (
-    <div>
-      Index
-    </div>
-  );
-};
+export const PostCard = (props: PostCardProps): React.ReactElement => (
+  <div className={style.postCard} {...props} />
+);
 
-export default Index;
+export type PostCardHeaderProps = ComponentPropsWithRef<'div'>
+
+export const PostCardHeader = (props: PostCardHeaderProps): React.ReactElement => (
+  <div className={style.postCard__header} {...props} />
+);
+
+export type PostCardCoverImageProps = GatsbyImageProps
+
+export const PostCardCoverImage = (props: PostCardCoverImageProps): React.ReactElement => (
+  <GatsbyImage className={style.postCard__image} {...props} />
+);
+
+export type PostCardCreditProps = ComponentPropsWithRef<'div'>
+
+export const PostCardCredit = (props: PostCardCreditProps): React.ReactElement => (
+  <div className={style.postCard__credit_wrap}>
+    <span className={style.postCard__credit} {...props} />
+  </div>
+);
+
+export type PostCardContainerProps = ComponentPropsWithRef<'div'>
+
+export const PostCardContainer = (props: PostCardContainerProps): React.ReactElement => (
+  <div className={style.postCard__container} {...props} />
+);
+
+export type PostCardTitleProps = ComponentPropsWithRef<'h2'>
+
+export const PostCardTitle = (props: PostCardTitleProps): React.ReactElement => (
+  <h2 className={style.postCard__title} {...props} />
+);
+
+export type PostCardContentProps = ComponentPropsWithRef<'p'>
+
+export const PostCardContent = (props: PostCardContentProps): React.ReactElement => (
+  <p className={style.postCard__content} {...props} />
+);
