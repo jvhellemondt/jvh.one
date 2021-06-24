034d27a1d833cf8856cdf897ddb6028636d159a1
Jens van Hellemondt <me@jvh.one>
2021-06-24 08:05:08 +0200

    feat: post template

--- src/templates/posts/index.tsx
+++ src/templates/posts/index.tsx
@@ -1,21 +1,67 @@
 import React, { ReactElement } from 'react';
-import { graphql, Link } from 'gatsby';
+import { graphql } from 'gatsby';
+import { FluidObject } from 'gatsby-image';
 import { MarkdownRemark } from '../../../graphql-types';
 import Layout from '../../layouts';
 
+import * as style from './style.module.scss';
+import {
+  PostCard,
+  PostCardContainer,
+  PostCardCoverImage,
+  PostCardHeader,
+  PostCardMeta,
+  PostCardTags
+} from '../../components/base/PostCard';
+
 export type PostQuery = {
-  data:{ markdownRemark: MarkdownRemark}
+  data: { markdownRemark: MarkdownRemark }
 }
 
 export default function Post({ data }: PostQuery): ReactElement {
   const post = data.markdownRemark;
+  const date = post.frontmatter?.date || '';
+  const title = post.frontmatter?.title || '';
+  const tags = post.frontmatter?.tags || [''];
+  const twitter = post.frontmatter?.tweet;
+  const published = post.frontmatter?.published || false;
+  const image = post.frontmatter?.cover_image;
+  const fluid = image?.childImageSharp?.fluid;
 
   return (
     <Layout subtitle={post.frontmatter?.title} withAuthor="bottom">
-      <h1>{post?.frontmatter?.title}</h1>
-      <small>{post?.frontmatter?.date}</small>
-      {/* eslint-disable-next-line react/no-danger */}
-      <div dangerouslySetInnerHTML={{ __html: `${post.html}` }} />
+      <div>
+        <h1 className={style.postTitle}>
+          {post?.frontmatter?.title}
+        </h1>
+
+        <PostCardMeta published={published} date={date} />
+      </div>
+
+      <PostCardContainer>
+        <PostCard>
+          {image && fluid && (
+            <PostCardHeader>
+              <PostCardCoverImage
+                alt={image?.name}
+                fluid={fluid as FluidObject}
+              />
+            </PostCardHeader>
+          )}
+
+          {/* eslint-disable-next-line react/no-danger */}
+          <div
+            className={style.post__content}
+            dangerouslySetInnerHTML={{ __html: `${post.html}` }}
+          />
+
+          <div>{`https://twitter.com/jvhellemondt/status/${twitter}`}</div>
+
+          <div className="post__footer">
+            <PostCardTags postTitle={title} tags={tags} />
+          </div>
+        </PostCard>
+      </PostCardContainer>
     </Layout>
   );
 }
@@ -26,7 +72,50 @@ export const query = graphql`
             frontmatter {
                 title
                 date
+                author
+                description
+                published
+                tags
+                tweet
+                cover_image {
+                    name
+                    childImageSharp {
+                        fluid(maxWidth: 1080, maxHeight: 400, fit: COVER) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
             }
+            timeToRead
         }
     }
 `;
+
+/*
+    <div className="post__content">
+      <VueRemarkContent/>
+    </div>
+
+    <div className="post__content" style="margin-top: 16px">
+      <div style="margin-bottom: 16px">
+        <span style="padding-right: 12px">Was dit leerzaam voor jou? Help mij dan het schrijven voort te zetten!</span>
+        <KofiButton uid="M4M3414BK" text="Trakteer mij op een kop koffie" color="#bf792a"/>
+      </div>
+      <div style="margin-bottom: 16px">
+        Mocht je vragen hebben, dan mag je die mij natuurlijk stellen! Je kunt ze stellen door een e-mail te sturen
+        naar
+        <a href="mailto:me@jvh.one">me@jvh.one</a>
+        <span v-if="$page.post.tweet"> of door te reageren op deze Tweet:</span>
+      </div>
+      <Tweet v-if="$page.post.tweet"
+             :id="$page.post.tweet"
+             :options="{ hideTread: false, hideMedia: false, align: 'center', omitScript: true }"
+             style="margin-bottom: 16px"/>
+    </div>
+
+    <div className="post__footer">
+      <PostTags :post="$page.post"/>
+      <PostDirectEdit :post="$page.post"/>
+    </div>
+  </div>
+ */
