d675767b7252e191acebbe2acb8fe2d01a720bae
Jens van Hellemondt <me@jvh.one>
2021-07-05 08:42:42 +0200

    refactor(tags): added PostCardTags to tags

--- src/templates/tags/index.tsx
+++ src/templates/tags/index.tsx
@@ -5,7 +5,7 @@ import {
   PostCard,
   PostCardContainer,
   PostCardContent,
-  PostCardMeta,
+  PostCardMeta, PostCardTags,
   PostCardTitle
 } from '../../components/base/PostCard';
 import Layout from '../../layouts';
@@ -29,16 +29,18 @@ export default function Tags({
       {edges.map(({ node: post }) => {
         const title = post.frontmatter?.title || '';
         const date = post.frontmatter?.date || '';
+        const tags = post.frontmatter?.tags || [''];
         const timeToRead = post.timeToRead || 0;
         const published = post.frontmatter?.published || false;
         const description = post.frontmatter?.description || '';
         const slug = post.fields?.slug;
         return (
-          <PostCardContainer>
+          <PostCardContainer key={title}>
             <PostCard tabIndex={0} onKeyPress={goToPost(slug)} onClick={goToPost(slug)}>
               <PostCardTitle>{title}</PostCardTitle>
               <PostCardContent>{description}</PostCardContent>
               <PostCardMeta published={published} date={date} timeToRead={timeToRead} />
+              <PostCardTags postTitle={title} tags={tags} />
             </PostCard>
           </PostCardContainer>
         );
@@ -48,31 +50,31 @@ export default function Tags({
 }
 
 export const query = graphql`
-    query TagsQuery($tag: String) {
-        allMarkdownRemark(
-            limit: 2000
-            sort: { fields: [frontmatter___date], order: DESC }
-            filter: { frontmatter: { tags: { in: [$tag] }, published: {eq: true} } }
-        ) {
-            totalCount
-            edges {
-                node {
-                    fields {
-                        slug
-                    }
-                    frontmatter {
-                        published
-                        author
-                        date(fromNow: true, formatString: "LL", locale: "nl_NL")
-                        description
-                        title
-
-                    }
-                    excerpt
-                    id
-                    timeToRead
-                }
-            }
-        }
-    }
-`;
+      query TagsQuery($tag: String, $limit: Int = 25) {
+          allMarkdownRemark(
+              limit: $limit
+              sort: { fields: [frontmatter___date], order: DESC }
+              filter: { frontmatter: { tags: { in: [$tag] }, published: {eq: true} } }
+          ) {
+              totalCount
+              edges {
+                  node {
+                      fields {
+                          slug
+                      }
+                      frontmatter {
+                          published
+                          author
+                          date(fromNow: true, formatString: "LL", locale: "nl_NL")
+                          description
+                          title
+                          tags
+                      }
+                      excerpt
+                      id
+                      timeToRead
+                  }
+              }
+          }
+      }
+  `;
