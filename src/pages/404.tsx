92601552ae38cae4b0ee0990d81b241587b7e103
Jens van Hellemondt <me@jvh.one>
2021-07-06 13:46:56 +0200

    refactor: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,58 +1,90 @@
 import React, { ReactElement } from 'react';
-import { Link } from 'gatsby';
+import { graphql, navigate } from 'gatsby';
+import {
+  PostCard,
+  PostCardContainer,
+  PostCardContent,
+  PostCardMeta,
+  PostCardTags,
+  PostCardTitle
+} from '../components/base/PostCard';
+import Layout from '../layouts';
+import { Maybe, Query } from '../../graphql-types';
+import * as style from './pages.module.scss';
+import Dropdown from '../components/base/Dropdown/Dropdown';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
+type NotFoundProps = { data: Query }
 
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
+const NotFoundPage = ({ data }: NotFoundProps): ReactElement => {
+  const {
+    edges,
+    distinct: allTags
+  } = data.allMarkdownRemark;
 
-// markup
-const NotFoundPage = (): ReactElement => (
-  <main style={pageStyles}>
-    <title>Not found</title>
-    <h1 style={headingStyles}>Page not found</h1>
-    <p style={paragraphStyles}>
-      Sorry
-      {' '}
-      <span role="img" aria-label="Pensive emoji">
-        😔
-      </span>
-      {' '}
-      we couldn’t find what you were looking for.
-      <br />
-      {process.env.NODE_ENV === 'development' ? (
-        <>
-          <br />
-          Try creating a page in
-          {' '}
-          <code style={codeStyles}>src/pages/</code>
-          .
-          <br />
-        </>
-      ) : null}
-      <br />
-      <Link to="/">Go home</Link>
-      .
-    </p>
-  </main>
-);
+  const goToPost = (slug: Maybe<string> | undefined) => () => navigate(slug || '/');
+  const handleTagSelect = (tag: string) => {
+    console.log({ tag });
+  };
+
+  return (
+    <Layout subtitle="Pagina niet gevonden..">
+      <div className={style.info}>
+        <p className="text-center full-width">Pagina niet gevonden.. </p>
+        <p>Ga terug naar de homepage, of klik op een artikel hieronder.</p>
+      </div>
+      {edges.map(({ node: post }) => {
+        const title = post.frontmatter?.title || '';
+        const excerpt = post.excerpt || '';
+        const tags = post.frontmatter?.tags || [''];
+        const date = post.frontmatter?.date || '';
+        const timeToRead = post.timeToRead || 0;
+        const published = post.frontmatter?.published || false;
+        const description = post.frontmatter?.description || '';
+        const slug = post.fields?.slug;
+        return (
+          <PostCardContainer key={slug}>
+            <PostCard tabIndex={0} onKeyPress={goToPost(slug)} onClick={goToPost(slug)}>
+              <PostCardTitle>{title}</PostCardTitle>
+              <PostCardContent>{description || excerpt}</PostCardContent>
+              <PostCardMeta published={published} date={date} timeToRead={timeToRead} />
+              <PostCardTags postTitle={title} tags={tags} />
+            </PostCard>
+          </PostCardContainer>
+        );
+      })}
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
+
+export const query = graphql`
+    query LatestPosts {
+        allMarkdownRemark(
+            limit: 3
+            sort: {fields: frontmatter___date, order: DESC}
+            filter: {frontmatter: {published: {eq: true}}}
+        ) {
+            totalCount
+            distinct(field: frontmatter___tags)
+            edges {
+                node {
+                    fields {
+                        slug
+                    }
+                    frontmatter {
+                        published
+                        author
+                        date(fromNow: true, formatString: "LL", locale: "nl_NL")
+                        description
+                        title
+                        tags
+                    }
+                    excerpt
+                    id
+                    timeToRead
+                }
+            }
+        }
+    }
+`;
